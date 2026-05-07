f15724d4f201d01ad827b73794d26bd772a74197
DevThiagoCBM <thiago999011@gmail.com>
2026-05-07 16:25:37 -0300

    ...

--- aula13/ex015/script.js
+++ aula13/ex015/script.js
@@ -15,27 +15,33 @@ function verificar() {
         img.setAttribute('id', 'foto')
         if (fsex[0].checked) {
             genero = 'Homem'
-            if (idade >= 0 && idade < 13) {
+            if (idade >= 0 && idade < 10) {
                 // Criança
                 img.setAttribute('src', 'pexels-raymart-arnino-1948344-13790576.jpg')
-            } else if (idade < 21 && idade > 15) {
+            } else if (idade < 21) {
                 // Jovem
-                img.setAttribute('src', '')
+                img.setAttribute('src', 'pexels-kema-12257911.jpg')
             } else if (idade < 50) {
                 // Adulto
+                img.setAttribute('src', 'pexels-jonathanborba-18293329.jpg')
             } else {
                 // Idoso
+                img.setAttribute('src', 'pexels-caleboquendo-7938623.jpg')
             }
         } else if (fsex[1].checked) { // CORREÇÃO: Adicionado o "if" aqui
             genero = 'Mulher'
             if (idade >= 0 && idade < 10) {
                 // Criança
+                img.setAttribute('src', 'pexels-eric-moura-859101902-28439169.jpg')
             } else if (idade < 21) {
                 // Jovem
+                img.setAttribute('src', 'pexels-artempodrez-7048346.jpg')
             } else if (idade < 50) {
                 // Adulto
+                img.setAttribute('src', 'istockphoto-1915382108-2048x2048.jpg')
             } else {
                 // Idoso
+                img.setAttribute('src', 'istockphoto-1592147563-2048x2048.jpg')
             }
         }
         res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
---
+++ aula14/node_ccf954b11f174.tmp.js
@@ -0,0 +1,17 @@
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c > 1)
+
+/*var c = 1
+while (c <= 6) {
+    
+    console.log(`Passo ${c}`)
+    c++ //c = c + 1
+}*/
+
+/*console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')*/
